10a0e23dca1ded9299942f522a3b52f3265a4be0
Joyce Lin <joycelin79@gmail.com>
2017-07-15 19:24:05 -0700

    establish base css and favicon

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import './css/reset.css';
+import './css/variables.css';
 import './css/index.css';
-import App from './javascripts/App';
-import registerServiceWorker from './javascripts/registerServiceWorker';
+
+import App from './compnents/App';
+import registerServiceWorker from './compnents/registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
